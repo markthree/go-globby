a2f0ca26339a375c21f6243cf36b97529be4813a
markthree <1801982702@qq.com>
2023-03-17 20:56:17 +0800

    chore: update bench

--- bench.mjs
+++ bench.mjs
@@ -1,21 +1,36 @@
 import { globby } from 'globby'
 import { globbyBin } from './npm/index.mjs'
 
-console.time('globbyBin')
-const result = await globbyBin([
+const binStartTime = Date.now()
+const binResult = await globbyBin([
 	'**/*.json',
 	'!package.json',
 	'!node_modules/**/*'
 ])
-console.timeEnd('globbyBin')
+const binDuration = Date.now() - binStartTime
 
-console.time('globby')
-const result2 = await globby([
+const nodeStartTime = Date.now()
+const nodeResult = await globby([
 	'**/*.json',
 	'!package.json',
 	'!node_modules/**/*'
 ])
-console.timeEnd('globby')
+const nodeDuration = Date.now() - nodeStartTime
 
-console.log('globbyBin', result)
-console.log('globby', result2)
+console.log(
+	`globby    - duration: ${
+		nodeDuration / 1000
+	}s; result: ${nodeResult}`
+)
+console.log(
+	`go-globby - duration: ${
+		binDuration / 1000
+	}s; result: ${binResult}`
+)
+
+console.log('\n')
+
+console.log(
+	'go-globby vs globby -',
+	(nodeDuration / binDuration).toFixed(2) + ' ↑'
+)
