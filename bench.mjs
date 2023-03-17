cf8885170812950366e5400cf2745bca7c4ba5b6
markthree <1801982702@qq.com>
2023-03-17 20:44:44 +0800

    fix: add postinstall

--- bench.mjs
+++ bench.mjs
@@ -2,11 +2,19 @@ import { globby } from 'globby'
 import { globbyBin } from './npm/index.mjs'
 
 console.time('globbyBin')
-const result = await globbyBin(['*.json', '!package.json'])
+const result = await globbyBin([
+	'**/*.json',
+	'!package.json',
+	'!node_modules/**/*'
+])
 console.timeEnd('globbyBin')
 
 console.time('globby')
-const result2 = await globby(['*.json', '!package.json'])
+const result2 = await globby([
+	'**/*.json',
+	'!package.json',
+	'!node_modules/**/*'
+])
 console.timeEnd('globby')
 
 console.log('globbyBin', result)
